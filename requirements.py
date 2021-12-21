1a70c0592979dd402f986ccebe2084ef135cdb6a
SpeedsMach5 <90051271+SpeedsMach5@users.noreply.github.com>
2021-12-21 16:54:46 -0600

    Delete requirements.py

--- requirements.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from datetime import date
-import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objects as go
