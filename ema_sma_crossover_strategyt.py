c685d6c958ddcd09c6463de083de0be9e9136af7
Quentin <quentin.s.reynolds@gmail.com>
2022-01-13 13:23:03 -0600

    added .ewma to ema/sma strat

--- ema_sma_crossover_strategyt.py
+++ ema_sma_crossover_strategyt.py
@@ -43,7 +43,7 @@ class MA_CrossOver(bt.Strategy):
     )
 
     def __init__(self):
-        sma_fast = self.p._movav(period=self.p.fast)#.ewm(span=10).mean()
+        sma_fast = self.p._movav(period=self.p.fast).ewma(span=10).mean()
         sma_slow = self.p._movav(period=self.p.slow)
         #sma_slow = self.p._movav(period=self.p.slow)
 
