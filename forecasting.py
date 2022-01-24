6bc5f0573916b0f45b1e1aac2f3125973a761fbd
Quentin <quentin.s.reynolds@gmail.com>
2022-01-23 23:47:12 -0600

    reset plot output

--- forecasting.py
+++ forecasting.py
@@ -6,15 +6,14 @@ def forecast(pricing_data, period):
     df_train = pricing_data[["Date", "Close"]]
     df_train = df_train.rename(columns={"Date":"ds", "Close": "y"})
 
-    m = Prophet(changepoint_prior_scale=0.5)
+    m = Prophet(changepoint_prior_scale=0.5, )
     m.fit(df_train)
     future = m.make_future_dataframe(periods=period, freq='D')
     forecast = m.predict(future)
 
     #forecast.tail())
 
-    fig1 = m.plot(forecast, xlabel="Date",ylabel="Price"); add_changepoints_to_plot(fig1.gca(),  m, forecast)
-    fig1 = m.plot[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+    fig1 = m.plot(forecast, xlabel="Date",ylabel="Price");add_changepoints_to_plot(fig1.gca(),  m, forecast)
     #a = add_changepoints_to_plot( m, forecast)
 
     fig2 = m.plot_components(forecast)
